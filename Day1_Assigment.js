76d0d074f7f039958655e3bc42a29e35a68b9102
Khushi <Khushirana1101@gmail.com>
2023-05-08 17:37:26 +0530

    first commit

---
+++ Day1_Assigment.js
@@ -0,0 +1,17 @@
+document.write("</br>");
+//printing name
+let myname = "Khushi"
+document.write("My Name is "+"<i>" +myname+"</i>")
+
+document.write("</br>");
+
+    //printing city
+   let city = "New Delhi"
+	document.write("My Birth City is "+ city);
+
+  document.write("</br></br>Printing through JS")
+  var hobbies=["ReadingBooks", "ListeningMusic","ArtCraft"]
+  document.write("Here are my hobbies: </br>")
+  document.write("<ul><li>"+ hobbies[1]+ "</li>")
+  document.write("<li>"+ hobbies[1]+ "</li><li>"+ hobbies[2] +"</li></ul>");
+
